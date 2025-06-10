c778790a07a3ea8a73e3947dd12e3eb5ac9692d8
AbhisDotNet <ag529256@gmail.com>
2025-06-11 02:11:12 +0530

    Removed unnecessary  using statements

--- DevTaskTracker.Application/Mappers/MappingProfile.cs
+++ DevTaskTracker.Application/Mappers/MappingProfile.cs
@@ -1,13 +1,7 @@
 ﻿using AutoMapper;
-using DevTaskTracker.Application.DTOs.AuthDtos;
 using DevTaskTracker.Application.DTOs.MemberDtos;
 using DevTaskTracker.Application.DTOs.TaskDtos;
 using DevTaskTracker.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevTaskTracker.Application.Mappers
 {
